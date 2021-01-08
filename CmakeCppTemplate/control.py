99a6c10463faccb87987da2f5ccb3fb763166f73
Kostiantyn Ziabrev <kziabrev@luxoft.com>
2021-01-08 19:27:24 +0200

    Update CmakeTemplate

--- CmakeCppTemplate/control.py
+++ CmakeCppTemplate/control.py
@@ -2,8 +2,7 @@ import os
 import sys
 import subprocess
 
-BUILD_DIR = "build"
-EXEC = "simple_project"
+BUILD_DIR = "_build"
 
 def default():
     if not os.path.exists(BUILD_DIR):
@@ -11,7 +10,19 @@ def default():
 
     subprocess.run("cmake ..", cwd=BUILD_DIR, shell=True)
     subprocess.run("make", cwd=BUILD_DIR, shell=True)
-    subprocess.run("./" + EXEC, cwd=BUILD_DIR, shell=True)
+    subprocess.run("program/program", cwd=BUILD_DIR, shell=True)
+
+def test():
+    if not os.path.exists(BUILD_DIR):
+        os.mkdir(BUILD_DIR)
+
+    subprocess.run("cmake ..", cwd=BUILD_DIR, shell=True)
+    subprocess.run("make", cwd=BUILD_DIR, shell=True)
+    subprocess.run("testrunner/testrunner", cwd=BUILD_DIR, shell=True)
+
 
 if len(sys.argv) == 1:
     default()
+else:
+    locals()[sys.argv[1]](*sys.argv[2:])
+
